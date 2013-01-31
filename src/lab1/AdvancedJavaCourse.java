387391f4cd3ca6ff0ea327c6cfca7f583535b8f1
lhendrickson3@my.wctc.edu <>
2013-01-31 17:02:38 -0600

    Created a Runner class, removed unneeded code, and fixed a main method

--- src/lab1/AdvancedJavaCourse.java
+++ src/lab1/AdvancedJavaCourse.java
@@ -16,11 +16,6 @@ public class AdvancedJavaCourse extends ProgrammingCourse {
         this.setCourseNumber(courseNumber);
     }
 
-    public String getCapitalizedCourseName() {
-        return this.getCourseName().toUpperCase();
-    }
-
-
     public String getPrerequisites() {
         return prerequisites;
     }
@@ -32,31 +27,6 @@ public class AdvancedJavaCourse extends ProgrammingCourse {
             System.exit(0);
         }
         this.prerequisites = prerequisites;
-    }
-
-    public void setCredits(double credits) {
-        if(credits < 0.5 || credits > 4.0) {
-            JOptionPane.showMessageDialog(null,
-                    "Error: credits must be in the range 0.5 to 4.0");
-            System.exit(0);
-        }
-        this.setCredits(credits);
-    }
-
-    public String getCourseName() {
-        return courseName;
-    }
-
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
-
-    public String getCourseNumber() {
-        return courseNumber;
-    }
-
-    public void setCourseNumber(String courseNumber) {
-        this.courseNumber = courseNumber;
     }
     
 }
--- src/lab1/IntroJavaCourse.java
+++ src/lab1/IntroJavaCourse.java
@@ -1,48 +1,33 @@
 package lab1;
 
+import javax.swing.JOptionPane;
+
 /**
  * Describe responsibilities here.
  *
  * @author      your name goes here
  * @version     1.00
  */
-public class IntroJavaCourse extends ProgrammingCourse{
+public class IntroJavaCourse extends ProgrammingCourse {
     
     private String prerequisites;
 
     public IntroJavaCourse(String courseName, String courseNumber) {
-        this.courseName = courseName;
-        this.courseNumber = courseNumber;
+        this.setCourseName(courseName);
+        this.setCourseNumber(courseNumber);
     }
-
-    public String getCourseNumber() {
-        return courseNumber;
-    }
-
-    public void setCourseNumber(String courseNumber) {
-        this.courseNumber = courseNumber;
-    }
-
-    public double getCredits() {
-        return credits;
-    }
-
 
     public String getPrerequisites() {
         return prerequisites;
     }
 
     public void setPrerequisites(String prerequisites) {
-        this.prerequisites = prerequisites;
-    }
-
-        public void setCredits(double credits) {
-        if(credits < 0 || credits > 5.0) {
-            System.out.println(
-                    "Error: credits must be in the range 0.5 to 4.0");
+        if(prerequisites == null || prerequisites.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: prerequisites cannot be null of empty string");
             System.exit(0);
         }
-        this.setCredits(credits);
+        this.prerequisites = prerequisites;
     }
 
 }
